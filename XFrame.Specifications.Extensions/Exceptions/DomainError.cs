1d73ef234dd40573c8c793d740413c453d49f5eb
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-11 14:33:15 +0200

    Add project files.

---
+++ XFrame.Specifications.Extensions/Exceptions/DomainError.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.Annotations;
+
+namespace XFrame.Specifications.Extensions.Exceptions
+{
+    public class DomainError : Exception
+    {
+        protected DomainError(string message)
+            : base(message)
+        {
+        }
+
+        protected DomainError(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        [StringFormatMethod("format")]
+        public static DomainError With(string format, params object[] args)
+        {
+            var message = args.Length <= 0
+                ? format
+                : string.Format(format, args);
+            return new DomainError(message);
+        }
+
+        [StringFormatMethod("format")]
+        public static DomainError With(Exception innerException, string format, params object[] args)
+        {
+            var message = args.Length <= 0
+                ? format
+                : string.Format(format, args);
+            return new DomainError(message, innerException);
+        }
+    }
+}
